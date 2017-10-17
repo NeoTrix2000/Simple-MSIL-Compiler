412ba8ad4daab45e29ec878f74b535423894a676
LADSoft <touchstone222@runbox.com>
2017-10-17 19:33:55 -0400

    C11 bug fixes

--- preproc/preproc.c
+++ preproc/preproc.c
@@ -1627,12 +1627,16 @@ char *fullqualify(char *string)
         p = buf + strlen(buf);
         if (!strncmp(q, ".\\", 2))
             q += 2;
+        p--;
         while (!strncmp(q, "..\\", 3))
         {
             q += 3;
             while (p > buf &&  *p != '\\')
                 p--;
+            if (p > buf)
+                p--;
         }
+	p++;
         *p++ = '\\';
         strcpy(p, q);
         return buf;
