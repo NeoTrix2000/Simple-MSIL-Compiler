00fc022c191298bdec48fdfd71752f3a959aaffc
LADSoft <touchstone222@runbox.com>
2018-04-14 06:04:03 -0400

    update for install

--- occil/ms.bat
+++ occil/ms.bat
@@ -1,1 +1,8 @@
 msbuild /Property:configuration=Release /Property:platform=x86
+mkdir \occil
+xcopy /S /Y ..\installartifacts\*.* \occil  
+mkdir \occil\DotNetPELib
+xcopy /S /Y ..\netil\netlib\*.* \occil\DotNetPELib
+xcopy Release\*.exe \occil\bin
+xcopy Release\*.dll \occil\bin
+iscc ..\occil.iss
